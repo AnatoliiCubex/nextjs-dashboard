b54725fd9dba04b032ebba7fd13d926ca41bf2e4
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2024-09-13 12:30:51 +0300

    fixed wrong import pathes

--- app/ui/components/dashboard/latest-invoices.tsx
+++ app/ui/components/dashboard/latest-invoices.tsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
-import { lusitana } from "~/app/ui/fonts";
+import { lusitana } from "~/app/ui/fonts/fonts";
 import { LatestInvoice } from "~/app/lib/definitions";
 export default async function LatestInvoices({
   latestInvoices,
@@ -9,11 +9,11 @@ export default async function LatestInvoices({
   latestInvoices: LatestInvoice[];
 }) {
   return (
-    <div className='flex w-full flex-col md:col-span-4'>
+    <div className='flex flex-col md:col-span-4 w-full'>
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className='flex grow flex-col justify-between rounded-xl bg-gray-50 p-4'>
+      <div className='flex flex-col justify-between bg-gray-50 p-4 rounded-xl grow'>
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
         {/* <div className="bg-white px-6">
@@ -22,7 +22,7 @@ export default async function LatestInvoices({
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  'flex flex-row justify-between items-center py-4',
                   {
                     'border-t': i !== 0,
                   },
@@ -37,10 +37,10 @@ export default async function LatestInvoices({
                     height={32}
                   />
                   <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
+                    <p className="font-semibold text-sm md:text-base truncate">
                       {invoice.name}
                     </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
+                    <p className="sm:block hidden text-gray-500 text-sm">
                       {invoice.email}
                     </p>
                   </div>
@@ -54,9 +54,9 @@ export default async function LatestInvoices({
             );
           })}
         </div> */}
-        <div className='flex items-center pb-2 pt-6'>
-          <ArrowPathIcon className='h-5 w-5 text-gray-500' />
-          <h3 className='ml-2 text-sm text-gray-500 '>Updated just now</h3>
+        <div className='flex items-center pt-6 pb-2'>
+          <ArrowPathIcon className='w-5 h-5 text-gray-500' />
+          <h3 className='ml-2 text-gray-500 text-sm'>Updated just now</h3>
         </div>
       </div>
     </div>
