41309b0d6b9305fb0c8c8f50bc3f61babb9cd387
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2024-09-16 17:21:04 +0300

    added skeletons to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,10 +7,15 @@ import {
   fetchLatestInvoices,
   fetchRevenue,
 } from "~/app/lib/data";
+import { Suspense } from "react";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "~/app/ui/components/skeletons";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -34,8 +39,12 @@ export default async function Page() {
         />
       </div>
       <div className='gap-6 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 mt-6'>
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,10 +13,12 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+    console.log("Data fetch completed after 3 seconds.");
 
     return data.rows;
   } catch (error) {
@@ -38,6 +40,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     return latestInvoices;
   } catch (error) {
     console.error("Database Error:", error);
